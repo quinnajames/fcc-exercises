896a34a3bf3ff4da0aee0ca647123440218492ab
volucrisscr <quinn@volucris.space>
2016-09-07 22:06:59 -0400

    Added Intermediate Algorithm 1

---
+++ int-alg-01.js
@@ -0,0 +1,10 @@
+// FreeCodeCamp Intermediate Algorithm 1 - Finders Keepers
+// Solution by Quinn James
+
+function findElement(arr, func) {
+  var filtered = arr.filter(func);
+
+  return filtered[0];
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
