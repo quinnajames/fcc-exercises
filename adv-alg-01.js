7d4c64add55066a2e487bdf74207587ad42e1401
volucrisscr <quinn@volucris.space>
2016-09-08 15:34:44 -0400

    (minor) Added note about country code to FCC Adv 1

--- adv-alg-01.js
+++ adv-alg-01.js
@@ -3,6 +3,7 @@
 
 function telephoneCheck(str) {
   var regexp = new RegExp(/(^(1\s)?[0-9]{10}$)|(^((1\s)?([0-9]{3}(\s|-))|^((1\s)|1)?\([0-9]{3}\)(\s|-)?)[0-9]{3}(\s|-)[0-9]{4}$)/gm);
+  // Country code is optional but has to be 1
   // First part of regexp: accept lines consisting solely of 10 numbers
   // Second part of regexp: accept either numbers with () or without () around the area code [but not just ( or )]
   // Within both subparts of second part, accept optional spaces or dashes between the components of the phone #
