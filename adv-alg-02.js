280887076440551ec959bc828ec6bd03039822d4
volucrisscr <quinn@volucris.space>
2016-09-26 16:58:10 -0400

    Remove accidental globals (and redudnant sample usage) in Adv. Alg #2

--- adv-alg-02.js
+++ adv-alg-02.js
@@ -7,10 +7,10 @@
 /* Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}). */
 
 function doSymmetricDifference(leftArray, rightArray) {
-  leftArray_filtered = leftArray.filter(function(element, index, array) {
+  var leftArray_filtered = leftArray.filter(function(element, index, array) {
     return (rightArray.indexOf(element) === -1);
   });
-  rightArray_filtered = rightArray.filter(function(element, index, array) {
+  var rightArray_filtered = rightArray.filter(function(element, index, array) {
     return (leftArray.indexOf(element) === -1);
   });
 
@@ -43,12 +43,3 @@ function sym(args) {
   });
 
 }
-
-document.write(sym([1, 2, 3], [5, 2, 1, 4])); // should be [3,4,5]
-document.write("<br />");
-document.write(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])) //should return [1, 4, 5].
-document.write("<br />");
-document.write(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])) //should return [2, 3, 4, 6, 7].
-document.write("<br />");
-document.write(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])) // should return [1, 2, 4, 5, 6, 7, 8, 9].
-document.write("<br />");
